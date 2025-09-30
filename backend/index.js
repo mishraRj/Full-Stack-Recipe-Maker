dabe1af04b7aa679efbacf789158bc2f2c2c65b7
Rajat Mishra <rajatmishra392003@gmail.com>
2025-09-30 13:12:26 +0530

    Prepairing for deployment 3

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use("/", recipeRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 }
